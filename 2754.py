04b99e9c5eb7294f878503fd72762d64ce1fad15
Uzivert47 <163652177+Uzivert47@users.noreply.github.com>
2024-03-22 04:31:57 +0900

    Add files via upload

---
+++ 2754.py
@@ -0,0 +1,26 @@
+Grade=list(input())
+
+if Grade[0]=="F":
+    print(0.0)
+
+else:
+    if Grade[0]=="A":
+        score=4.0
+    elif Grade[0]=="B":
+        score=3.0
+    elif Grade[0]=="C":
+        score=2.0
+    elif Grade[0]=="D":
+        score=1.0
+    else:
+        print("ㅗ")
+
+    if len(Grade)>1:
+
+        if Grade[1]=="-":
+                print(score-0.3)
+        elif Grade[1]=="+":
+                print(score+0.3)
+        else:
+                print(score)
+    else:print("ㅗ")
